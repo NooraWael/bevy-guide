0a1bc4bf19a038af80f7447a1be6490b4b241c68
Noora <69541927+NooraWael@users.noreply.github.com>
2025-03-02 02:16:56 +0300

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Sidebar from '@/app/components/sidebar';
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Analytics/>
       <body className={inter.className}>
         <div className="flex">
           <div className="sticky top-0 h-screen">
